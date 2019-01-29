cda2a3c444bf4e4ffe438270ec27d236d34bfd64
Charles Chong <user@Charless-MacBook-Pro.local>
2019-01-29 14:05:25 -0800

    Deploy

--- publicreactnativeapp/src/redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import stylesReducer from './stylesReducer';
-
-const rootReducer = combineReducers({
-    style:stylesReducer
-  })
-  
-  export default rootReducer;
